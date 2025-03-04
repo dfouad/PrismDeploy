21d87050b54bab157f99759fb2300fb4bc68b260
dfouad <eng.doaa.fouad@gmail.com>
2025-03-04 11:26:44 +0200

    adding logo and students app

--- Project/settings.py
+++ Project/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'AdminPanel',
+    'Students',
 ]
 
 MIDDLEWARE = [
@@ -122,6 +123,8 @@ STATIC_URL = 'static/'
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static')
 ]
+#STATIC_ROOT = os.path.join(BASE_DIR , 'static')
+#STATICFILES_DIRS = [os.path.join(BASE_DIR , 'dodo/static')]
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
