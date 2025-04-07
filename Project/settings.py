fc13134834cfdc6b3136bee15096dcdb78831ed4
dfouad <eng.doaa.fouad@gmail.com>
2025-04-07 11:07:04 +0200

    some fix for hosting

--- Project/settings.py
+++ Project/settings.py
@@ -81,10 +81,10 @@ WSGI_APPLICATION = 'Project.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'NAME': str(BASE_DIR / 'db.sqlite3'), 
     }
 }
-
+# 'NAME': BASE_DIR / 'db.sqlite3',
 
 # Password validation
 # https://docs.djangoproject.com/en/5.1/ref/settings/#auth-password-validators
@@ -121,13 +121,13 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static')
 ]
 #STATIC_ROOT = os.path.join(BASE_DIR , 'static')
-#STATICFILES_DIRS = [os.path.join(BASE_DIR , 'dodo/static')]
+#STATICFILES_DIRS = [os.path.join(BASE_DIR , 'Project/static')]
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
@@ -143,3 +143,4 @@ GRAPH_MODELS = {
 
 
 #ALLOWED_HOSTS = ['192.168.1.50']
+ALLOWED_HOSTS = ["dfouad.pythonanywhere.com"]
--- Students/views.py
+++ Students/views.py
@@ -6,22 +6,22 @@ def courses(request):
     active_courses=Courses.objects.filter(is_active = "Active Now")
     soon_courses=Courses.objects.filter(is_active = "Coming Soon")
     context={'activecoursesList':active_courses,'sooncoursesList':soon_courses}
-    return render (request,'studentsapp/courses.html',context)
+    return render (request,'StudentsApp/courses.html',context)
 
 
 def courseDetails(request,course_id):
     course_details=Courses.objects.get(id=course_id)
     context={'course':course_details}
-    return render (request,'studentsapp/courseDetails.html',context)
+    return render (request,'StudentsApp/courseDetails.html',context)
 
 
 def courseByAge(request,course_age):
     filtered_courses_by_age = Courses.objects.filter(age=course_age)
     context= {'filtered_courses_by_age': filtered_courses_by_age}
-    return render(request,'studentsapp/filteredCourses.html',context )
+    return render(request,'StudentsApp/filteredCourses.html',context )
 
 
 def courseByCategory(request,course_category):
     filtered_courses_by_category = Courses.objects.filter(category=course_category)
     context= {'filtered_courses_by_category': filtered_courses_by_category}
-    return render(request,'studentsapp/filteredCourses.html',context )
+    return render(request,'StudentsApp/filteredCourses.html',context )
