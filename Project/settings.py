863642631026d9dcc85879f829e1cf310dc25838
dfouad <eng.doaa.fouad@gmail.com>
2025-05-11 18:11:09 +0300

    1

--- Project/settings.py
+++ Project/settings.py
@@ -135,8 +135,8 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
-STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
-STATIC_ROOT = os.path.join(BASE_DIR , 'static_collected')
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static_origin')]
+STATIC_ROOT = os.path.join(BASE_DIR , 'static')
 
 #STATICFILES_DIRS = [os.path.join(BASE_DIR , 'Project/static')]
 # Default primary key field type
